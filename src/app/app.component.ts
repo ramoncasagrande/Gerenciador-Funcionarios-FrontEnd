c0a1d9bf1481a8231ece65542f117db6c7f4d5c6
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-26 19:38:50 -0300

    Configurações de Componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Funcionario } from './funcionario';
+import { FuncionarioService } from './funcionario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gerenciador';
+export class AppComponent implements OnInit {
+  public funcionarios: Funcionario[] = [];
+
+  constructor(private funcionarioService: FuncionarioService){}
+
+  ngOnInit() {
+      this.buscaFuncionarios();
+  }
+
+  public buscaFuncionarios(): void {
+    this.funcionarioService.listaFuncionarios().subscribe(
+      (response: Funcionario[]) => {
+        this.funcionarios = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+  
 }
