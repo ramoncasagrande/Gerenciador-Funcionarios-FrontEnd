5572dcd2240f0e068fe9e22ad0d1cbd5c4aece79
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-26 19:07:45 -0300

    Criados métodos no funcionario.Service

--- src/app/funcionario.service.ts
+++ src/app/funcionario.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Funcionario } from './funcionario';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,23 @@ export class FuncionarioService {
 
   constructor(private http: HttpClient) { }
 
-  public listaFuncionarios(): Observable<any> {
-    return this.http.get<any>(`${this.apiServeUrl}/funcionarios`);
+  public listaFuncionarios(): Observable<Funcionario[]> {
+    return this.http.get<Funcionario[]>(`${this.apiServeUrl}/funcionarios`);
   }
+
+  public adicionaFuncionarios(funcionario: Funcionario): Observable<Funcionario> {
+    return this.http.post<Funcionario>(`${this.apiServeUrl}/funcionarios`, funcionario);
+  }
+
+  public atualizaFuncionarios(funcionario: Funcionario): Observable<Funcionario> {
+    return this.http.put<Funcionario>(`${this.apiServeUrl}/funcionarios`, funcionario);
+  }
+
+  public excluiFuncionarios(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServeUrl}/funcionarios/${id}`);
+  }
+
+
+
+
 }
